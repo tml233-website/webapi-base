1f705d095c6028929f0fb93f1e8cd74745694778
TML <houjunhao33@163.com>
2023-06-16 16:35:31 +0800

    Fix build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 import Application from "./Application";
 import Module from "./Modules/Module";
 import RouterModule from "./Modules/RouterModule";
+import { RouteHandler } from "./RouteHandler";
 import AsyncHandler from "express-async-handler";
 import { EndpointConfig, KeysConfig } from "./Config";
-export { Application, Module, RouterModule, AsyncHandler, EndpointConfig, KeysConfig };
+export { Application, Module, RouterModule, RouteHandler, AsyncHandler, EndpointConfig, KeysConfig };
