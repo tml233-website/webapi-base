01b62efac7c18d24e8829d31e07da0d77683d4aa
TML <houjunhao33@163.com>
2023-06-16 16:26:17 +0800

    Add RouteHandler

---
+++ Source/RouteHandler.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import core from "express-serve-static-core";
+
+type HandlerArg<P = core.ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = core.Query> = Parameters<express.RequestHandler<P, ResBody, ReqBody, ReqQuery>>;
+type HandlerResult = void | Promise<void>;
+export type RouteHandler = (...args: HandlerArg) => HandlerResult;
--- Source/index.ts
+++ Source/index.ts
@@ -3,6 +3,7 @@ import Application from "./Application";
 import Module from "./Modules/Module";
 import RouterModule from "./Modules/RouterModule";
 
+import { RouteHandler } from "./RouteHandler";
 import AsyncHandler from "express-async-handler";
 
 import { EndpointConfig, KeysConfig } from "./Config";
@@ -14,6 +15,7 @@ export {
     Module,
     RouterModule,
 
+    RouteHandler,
     AsyncHandler,
 
     EndpointConfig,
